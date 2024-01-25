d38764fdfe4b73ab82ef8a2c1237efaa5a33dafa
mgberrysd <mgberrysd@gmail.com>
2024-01-25 15:58:18 -0800

    added get and post routes for the api using the helper utils, will change to fs

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const api = require('./public/assets/js/index');
-
+// const api = require('./public/assets/js/index');
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('./helpers/fsUtils');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,41 +15,40 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data) =>
-        res.json(JSON.parse(data))
-    );
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 app.post('/api/notes', (req, res) => {
     console.log(req.body);
 
-    const { username, topic, tip } = req.body;
+    const { title, text} = req.body;
 
     if (req.body) {
-        const newTip = {
-            username,
-            tip,
-            topic,
-            tip_id: uuidv4(),
+        const newTask = {
+            title,
+            text,
+            task_id: uuidv4(),
         };
 
-        readAndAppend(newTip, './db/db.json');
-        res.json(`Tip added successfully`);
+        readAndAppend(newTask, './db/db.json');
+        res.json(`Task added successfully`);
     } else {
-        res.error('Error in adding tip');
+        res.error('Error in adding task');
     }
 });
+
+app.get('/*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 
 app.listen(PORT, () =>
